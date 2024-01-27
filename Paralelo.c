e48555ca6ab15a48c051460fb4dee13650b3c1ac
Edgar Lopez <edgar.lopez.v99@gmail.com>
2024-01-27 09:41:57 -0600

    Update Paralelo.c

--- Paralelo.c
+++ Paralelo.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <omp.h>
 
 void generateRandomArray(int array[], int size) {
     for (int i = 0; i < size; i++) {
-        array[i] = rand();
+        //Reducir las opciones a numeros menores a 100
+        array[i] = rand() % 100;
     }
 }
 
 int main() {
+    #pragma omp parallel
+    {
+        int id = omp_get_thread_num();
+        printf("Hola desde el hilo %d\n", id);
+    }
+
     srand(time(NULL));
     const int size = 1000;
 
     int arrayA[size];
-    generateRandomArray(arrayA, array_size);
+    generateRandomArray(arrayA, size);
 
     int arrayB[size];
-    generateRandomArray(arrayB, array_size);
+    generateRandomArray(arrayB, size);
 
 
     printf("Array 1:\n");
     for (int i = 0; i < 10; i++) {
-        printf("%d ", array1[i]);
+        printf("%d ", arrayA[i]);
     }
 
     printf("\n\nArray 2:\n");
     for (int i = 0; i < 10; i++) {
-        printf("%d ", array2[i]);
+        printf("%d ", arrayB[i]);
     }
 
     return 0;
