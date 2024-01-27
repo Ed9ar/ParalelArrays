5d93adefc9f7185281a5112953ba8ef0c2883988
Edgar Lopez <edgar.lopez.v99@gmail.com>
2024-01-27 10:02:08 -0600

    Update Paralelo.c

--- Paralelo.c
+++ Paralelo.c
@@ -3,6 +3,32 @@
 #include <time.h>
 #include <omp.h>
 
+void sumArrays(int arrayA[], int arrayB[], int arrayR[], int size) {
+    int thread_num = 0;
+
+    #pragma omp parallel sections
+    {
+        // Primera mitad
+        #pragma omp section
+        {
+            thread_num = omp_get_thread_num();
+            printf("Hilo %d que suma la primera mitad \n", thread_num);
+            for (int i = 0; i < size / 2; i++) {
+                arrayR[i] = arrayA[i] + arrayB[i];
+            }
+        }
+        //Segunda mitad
+        #pragma omp section
+        {
+            thread_num = omp_get_thread_num();
+            printf("Hilo %d que suma la segunda mitad \n", thread_num);
+            for (int i = size / 2; i < size; i++) {
+                arrayR[i] = arrayA[i] + arrayB[i];
+            }
+        }
+    }
+}
+
 void generateRandomArray(int array[], int size) {
     for (int i = 0; i < size; i++) {
         //Reducir las opciones a numeros menores a 100
@@ -11,11 +37,6 @@ void generateRandomArray(int array[], int size) {
 }
 
 int main() {
-    #pragma omp parallel
-    {
-        int id = omp_get_thread_num();
-        printf("Hola desde el hilo %d\n", id);
-    }
 
     srand(time(NULL));
     const int size = 1000;
@@ -26,16 +47,26 @@ int main() {
     int arrayB[size];
     generateRandomArray(arrayB, size);
 
+    int arrayR[size];
+    sumArrays(arrayA, arrayB, arrayR, size);
 
-    printf("Array 1:\n");
+    printf("Array A:\n");
     for (int i = 0; i < 10; i++) {
         printf("%d ", arrayA[i]);
     }
 
-    printf("\n\nArray 2:\n");
+    printf("\n\nArray B:\n");
     for (int i = 0; i < 10; i++) {
         printf("%d ", arrayB[i]);
     }
 
+    printf("\n\nArray R:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", arrayR[i]);
+    }
+
     return 0;
 }
+
+
+
